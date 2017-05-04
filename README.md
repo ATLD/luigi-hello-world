392f1605bdf063ece85fb8683e61e6e8e5f1081e
Aaron <aaron.k@voxy.com>
2017-05-04 08:59:07 -0400

    Add HelloWorldTask

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is just a simple 'Hello World' example to demonstrate the use of Luigi.
 
-It will create a file that says "Hello", and another that says "World". 
+It will create a file that says "Hello", another that says "World", and concatenate the contents of those files into a third that says "Hello World". 
 
 Usage:
 
@@ -12,6 +12,5 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 luigid --background --port=8082 --logdir=logs
-python hello_world.py HelloTask
-python hello_world.py WorldTask
+python hello_world.py HelloWorldTask
 ```
--- hello_world.py
+++ hello_world.py
@@ -25,5 +25,23 @@ class WorldTask(luigi.Task):
         return luigi.LocalTarget('world.txt')
 
 
+class HelloWorldTask(luigi.Task):
+    def run(self):
+        sleep(60)
+        with open('hello.txt', 'r') as hello_file:
+            hello = hello_file.read()
+        with open('world.txt', 'r') as world_file:
+            world = world_file.read()
+        with open('hello_world.txt', 'w') as output_file:
+            content = '{} {}!'.format(hello, world)
+            output_file.write(content)
+            output_file.close()
+
+    def requires(self):
+        return [HelloTask(), WorldTask()]
+
+    def output(self):
+        return luigi.LocalTarget('hello_world.txt')
+
 if __name__ == '__main__':
    luigi.run()

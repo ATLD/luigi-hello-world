fc59ee2f0ce42be482567de83fb3fefe40b2d438
Aaron <aaron.k@voxy.com>
2017-05-04 08:52:42 -0400

    Simulate a long-running task with sleep

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,11 @@
+from time import sleep
+
 import luigi
 
 
 class HelloTask(luigi.Task):
     def run(self):
+        sleep(60)
         with open('hello.txt', 'w') as hello_file:
             hello_file.write('Hello')
             hello_file.close()
